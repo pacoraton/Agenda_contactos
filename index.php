dbc0506d5fb32a0516a54ab76575e66b0951d799
PacoRaton <pacocasino1@hotmail.com>
2019-07-18 18:24:11 -0500

    Se agregan los campos para añadir un nuevo contacto

--- index.php
+++ index.php
@@ -2,9 +2,31 @@
 
  <div class='cabecera' >
      <h1>Agenda Contactos</h1>
-
  </div>
 
+ <div class="bg-amarillo contenedor sombra ">
+    <form id="contacto" action="#">
+        <legend>Añada un contacto. <span>Todos los campos son obligatorios</span></legend>
+        
+        <div class="campos">
+            <div class="campo">
+                <label for="nombre">Nombre: </label>        
+                <input type="text" id="nombre" placeholder="Nombre Contacto" >
+            </div> 
+            <div class="campo">
+                <label for="empresa">Empresa: </label>        
+                <input type="text" id="empresa" placeholder="Empresa" >
+            </div> 
+            <div class="campo">
+                <label for="telefono">Teléfono: </label>        
+                <input type="tel" id="telefono" placeholder="Teléfono">
+            </div>  
+            <div class="campo">        
+                <input type="submit" id="añadir" value="Añadir" >
+            </div> 
+        </div>
+    </form>
+ </div>
 
 
 
