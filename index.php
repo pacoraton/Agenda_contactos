3335334ea6e35d8906ed084e764b6afd688ff44c
PacoRaton <pacocasino1@hotmail.com>
2019-07-29 12:29:26 -0500

    Se crea el html para el listado de contactos (se agregan dos iconos de fontowesone

--- index.php
+++ index.php
@@ -30,5 +30,75 @@
  </div>
 
 
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contacto">
+        <h2>Contactos</h2>
+
+        <input type="text" id="buscar" class="buscador sombra" placeholder="Buscar Contactos..." >
+        <p class="total-contactos"><span>2</span>Contactos</p>
+
+
+        <div class="contenedor-tabla">
+
+            <table>
+              <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Teléfono</th>
+                        <th>Acciones</th>
+                    </tr>
+              </thead>
+              <tbody>
+                    <tr>
+                        <td>Francisco</td> 
+                        <td>Ninguna</td>
+                        <td>932344</td>
+                        <td>
+                            <a class="btn-editar btn" href="#">
+                                <i class="fas fa-pen-square"></i> 
+                            </a>
+                            <button data-id='1' type="button" class="btn-borrar btn">
+                                    <i class="fas fa-trash-alt"></i>
+                            </button>
+                        <td>
+                    </tr>
+                    <tr>
+                        <td>Francisco</td> 
+                        <td>Ninguna</td>
+                        <td>932344</td>
+                        <td>
+                            <a class="btn-editar btn" href="#">
+                                <i class="fas fa-pen-square"></i> 
+                            </a>
+                            <button data-id='1' type="button" class="btn-borrar btn">
+                                    <i class="fas fa-trash-alt"></i>
+                            </button>
+                        <td>
+                    </tr>
+                    <tr>
+                        <td>Francisco</td> 
+                        <td>Ninguna</td>
+                        <td>932344</td>
+                        <td>
+                            <a class="btn-editar btn" href="#">
+                                <i class="fas fa-pen-square"></i> 
+                            </a>
+                            <button data-id='1' type="button" class="btn-borrar btn">
+                                    <i class="fas fa-trash-alt"></i>
+                            </button>
+                        <td>
+                    </tr>
+              </tbody>
+            </table>
+
+        </div>
+
+    </div>
+
+
+
+</div>
+
 
 <?php include 'inc/layout/header.php'; ?>
